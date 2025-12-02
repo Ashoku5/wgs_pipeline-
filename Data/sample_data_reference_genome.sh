51548236412eebdbeb955a6b107062b1d8f5319a
Ashoku5 <ashokulligundam@gmail.com>
2025-12-02 15:23:24 +0530

    Update sample_data_reference_genome.sh

--- Data/sample_data_reference_genome.sh
+++ Data/sample_data_reference_genome.sh
@@ -1,8 +1,10 @@
+mkdir -p reads
 # data from SRA 
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR154/091/ERR15465491/ERR15465491.fastq.gz
+wget -nc -P reads/ ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR154/091/ERR15465491/ERR15465491.fastq.gz
 
+mkdir -p reference
 # reference genome from Ensembl Genomes (Fungi).
-wget  ftp://ftp.ensemblgenomes.org/pub/fungi/release-62/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz
+wget -P reference/ ftp://ftp.ensemblgenomes.org/pub/fungi/release-62/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz
 
 # Unzip compressed file 
-gunzip Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz
+gunzip reference/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz
